edc6796e42d6ac9927fca56d847b0e8300ba185e
malek.haoues-rhaiem <malek.haoues-rhaiem@tu-dortmund.de>
2025-06-11 20:10:05 +0200

    Kdoc for startgame(),endgame(),saveGame(),playTile() added

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -4,14 +4,26 @@ import entity.NovaLunaGame
 import entity.Player
 
 class GameService(private val rootService: RootService) : AbstractRefreshingService() {
-
+    /**
+     * Starts a new Nova Luna game with the given players and simulation speed.
+     * the game state + drawpile + tile track are initialized.
+     * Triggers [refreshAfterGameStarted] to update the UI with the initial game state.
+     * @param players The list of players participating in the game (2 to 4 players).
+     * @param simulationSpeed The speed of the game simulation (0 to 10).
+     * @throws IllegalStateException If a game is already running.
+     * @throws IllegalArgumentException or if the simulation speed is greater than 10.
+     * @throws IllegalArgumentException If the number of players is not between 2 and 4
+     */
     fun startNewGame(players : List<Player>, simulationSpeed : Int) {
 
         // überprüfe, ob Anzahl der Spieler passt (2 bis 4)
         require(players.size in 2..4) { "Spieleranzahl muss zwischen 2 und 4 sein." }
 
         require(simulationSpeed < 11) { "SimulationSpeed darf maximal 10 sein" }
-
+        // Überprüfe, ob bereits ein Spiel läuft
+        if (rootService.currentGame != null) {
+            throw IllegalStateException("A game is already running.")
+        }
         // Initializing the draw pile
         val drawPile = rootService.tileLoader.readTiles().shuffled().toMutableList()
 
@@ -37,5 +49,30 @@ class GameService(private val rootService: RootService) : AbstractRefreshingServ
         rootService.currentGame = game
 
     }
+
+
+    /**
+    * Ends the current Nova Luna game by determining the winner,
+    * displaying player scores, and clearing the game state.
+    *
+    * This method is  called from [checkEndGame] when an end condition is met.
+    * Triggers [refreshAfterGameEnd] to update the UI with the winner name and players  scores  .
+    * @throws IllegalStateException if no game is currently active or the game is already ended
+    */
+    fun endGame(){}
+
+    /**
+     * Saves the current Nova Luna game state to a file or persistent storage.
+     *
+     * Only allowed in offline mode while a game is active
+     *
+     * @throws IllegalStateException if no game is active or the game is in network mode
+     * @throws IOException if an error occurs during saving
+     */
+    fun saveGame(){}
+
+
+
+
 
 }
--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -1,5 +1,19 @@
 package service
-
+import tools.aqua.bgw.util.Coordinate
 class PlayerActionService(private val rootService: RootService) : AbstractRefreshingService()  {
 
+    /**
+     * Allows the current player to select a tile from the Moon Wheel at the specified index
+     * and place it on their personal space  at the desired position .
+     * the Meeple is  then moved to that index.
+     * The player's time marker is advanced based on the tile's cost.
+     * Triggers [refreshAfterTilePlayed] to update the UI accordingly.
+     *
+     * @param moonwheelIndex The index of the tile to take from the Moon Wheel (must be one of the next 3 tiles).
+     * @param position The coordinate where the tile should be placed on the current player's space .
+     *
+     * @throws IllegalStateException If no game is active or it's not the current player's turn.
+     * @throws IllegalArgumentException If the selected tile is invalid or cannot be placed at the given position.
+     */
+    fun playTile( moonwheelIndex: Int, position: Coordinate){}
 }
