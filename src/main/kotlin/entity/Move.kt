4a86ba6d33221ca8cfb9b1f82e750c09d05cad48
Leo <leonore.ebbinghaus@tu-dortmund.de>
2025-06-11 19:59:17 +0200

    added KDoc

--- src/main/kotlin/entity/Move.kt
+++ src/main/kotlin/entity/Move.kt
@@ -1,5 +1,10 @@
 package entity
 import tools.aqua.bgw.util.Coordinate
-
+/**
+ * Represents a move in the game Nova Luna.
+ *
+ * @property tile The tile that will be moved.
+ * @property position The position the tile will be moved to.
+ */
 data class Move(val tile: Tile,
                 val position: Coordinate)
