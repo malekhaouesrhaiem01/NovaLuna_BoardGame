915178f1daad868b1e084a16601b0f7f188f0382
malek.haoues-rhaiem <malek.haoues-rhaiem@tu-dortmund.de>
2025-06-25 21:41:07 +0200

    added InitMessage+Turnmessage+fixes to networkclient

--- src/main/kotlin/service/message/InitMessage.kt
+++ src/main/kotlin/service/message/InitMessage.kt
@@ -1,4 +1,38 @@
 package service.message
 
-class InitMessage {
+import tools.aqua.bgw.net.common.GameAction
+import tools.aqua.bgw.net.common.annotations.GameActionClass
+
+/**
+ * Initial message sent by the host to the guest to synchronize the game state at the start.
+ *
+ * @property playerNames List of player names in turn order.
+ * @property activePlayerIndex Index of the player who should start the game.
+ * @property moonTokenPosition The index of the moon token on the track (0–11).
+ * @property tileTrack The list of tile IDs currently on the tile track (may include null at the moon token).
+ * @property drawPile The list of remaining tile IDs in the draw pile (top to bottom).
+ */
+@GameActionClass
+data class InitMessage(
+    val playerNames: List<String>,
+    val activePlayerIndex: Int,
+    val moonTokenPosition: Int,
+    val tileTrack: List<Int?>,
+    val drawPile: List<Int>
+) : GameAction() {
+
+    /**
+     * String representation for debugging and logging purposes.
+     */
+    override fun formatMessage(): String {
+        return """
+            InitMessage(
+                playerNames=$playerNames,
+                activePlayerIndex=$activePlayerIndex,
+                moonTokenPosition=$moonTokenPosition,
+                tileTrack=$tileTrack,
+                drawPile=${drawPile.take(10)}...
+            )
+        """.trimIndent()
+    }
 }
--- src/main/kotlin/service/message/TurnMessage.kt
+++ src/main/kotlin/service/message/TurnMessage.kt
@@ -1,4 +1,27 @@
 package service.message
 
-class TurnMessage {
+import tools.aqua.bgw.net.common.GameAction
+import tools.aqua.bgw.net.common.annotations.GameActionClass
+
+/**
+ * Message representing a single player turn in Nova Luna.
+ * Sent from the active player to the opponent via the network.
+ *
+ * @property tileId The unique ID of the tile the player picked.
+ * @property x The x-coordinate where the tile was placed.
+ * @property y The y-coordinate where the tile was placed.
+ * @property refillTrack Whether the tile track was refilled after the move.
+ */
+@GameActionClass
+data class TurnMessage(
+    val tileId: Int,
+    val x: Int,
+    val y: Int,
+    val refillTrack: Boolean
+) : GameAction() {
+
+    override fun formatMessage(): String {
+        return "TurnMessage(tileId=$tileId, x=$x, y=$y, refillTrack=$refillTrack)"
+    }
 }
+// noch nicht fertig
